60c1edc1fe821db5efc06a36d3e34d82c898fabc
suluo <sam_suluo@163.com>
2019-08-08 20:48:05 +0800

    feat(models): loss

--- V1/Models.py
+++ V1/Models.py
@@ -4,7 +4,7 @@
 # File Name    : Models.py
 # Created By   : Suluo - sampson.suluo@gmail.com
 # Creation Date: 2019-07-13
-# Last Modified: 2019-08-08 19:24:39
+# Last Modified: 2019-08-08 20:47:39
 # Descption    :
 # Version      : Python 3.7
 ############################################
@@ -12,7 +12,7 @@ import argparse
 import torch
 import torch.nn as nn
 from Net.nn.Encoder.rnn import EncoderRNN
-
+from Net.nn.Loss.crf import CRFLoss
 
 class TaggerModel(nn.Module):
     def __init__(self, opt, vocab):
@@ -25,6 +25,7 @@ class TaggerModel(nn.Module):
             nn.Linear(opt.rnn_size, vocab.tgt_vocab_size),
             nn.LogSoftmax()
         )
+        self.criterion = CRFLoss(opt)
 
     def forward(self, src):
         """
@@ -37,3 +38,5 @@ class TaggerModel(nn.Module):
         context, hidden = self.encoder(src)
         out = self.generator(context)
         return out
+
+
